f6033c178842e6745e52aa34001e09319f7e4872
fabrilalvarez <fabrilalvarez@eniac23.danielcastelao.org>
2015-01-26 09:16:12 +0100

    version 2.1

--- src/control_versiones/Control_versiones.java
+++ src/control_versiones/Control_versiones.java
@@ -4,15 +4,33 @@ import java.util.Scanner;
 
 public class Control_versiones {
 
+    static double activar;
+    static double version = 2.1;
+
     public static void main(String[] args) {
         // TODO code application logic here
-        double version = 1.1;
-        double activar;
 
+        System.out.println("para ejecutar pulsa cualquier tecla");
         System.out.println("para ver versión pulsa 0");
+        System.out.println("para cerrar pulsa 1");
+        dato();
+        Versiones();
+
+        if (activar > 1) {
+            programa();
+        }
+    }
+
+    public static void programa() {
+        System.out.println(":::::::PROGRAMA:::::::");
+    }
+
+    public static void dato() {
         Scanner dato = new Scanner(System.in);
         activar = dato.nextDouble();
+    }
 
+    public static void Versiones() {
         if (activar == 0) {
             System.out.println("VERSION : " + version);
         }
@@ -20,14 +38,6 @@ public class Control_versiones {
         if (activar == 1) {
             System.out.println("Cerrado");
         }
-        
-        if(activar > 1){
-            System.out.println("");
-            
-        }
-        
-       
-
     }
 
 }
