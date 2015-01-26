b0b4b70f177e9d0ce6ed96779adcca1a1ea3cf62
fabrilalvarez <fabrilalvarez@eniac23.danielcastelao.org>
2015-01-26 09:04:54 +0100

    version 1.1

--- src/control_versiones/Control_versiones.java
+++ src/control_versiones/Control_versiones.java
@@ -1,12 +1,33 @@
-
 package control_versiones;
+
+import java.util.Scanner;
 
 public class Control_versiones {
 
     public static void main(String[] args) {
         // TODO code application logic here
-        double version=1;
-        System.out.println("VERSION : "+version);
+        double version = 1.1;
+        double activar;
+
+        System.out.println("para ver versión pulsa 0");
+        Scanner dato = new Scanner(System.in);
+        activar = dato.nextDouble();
+
+        if (activar == 0) {
+            System.out.println("VERSION : " + version);
+        }
+
+        if (activar == 1) {
+            System.out.println("Cerrado");
+        }
+        
+        if(activar > 1){
+            System.out.println("");
+            
+        }
+        
+       
+
     }
-    
+
 }
