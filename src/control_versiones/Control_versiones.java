53c4875eba6376cb68a0ed661461eeb060e5738b
fabrilalvarez <fabrilalvarez@eniac23.danielcastelao.org>
2015-01-26 09:49:25 +0100

    version 2.3

--- src/control_versiones/Control_versiones.java
+++ src/control_versiones/Control_versiones.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Control_versiones {
 
     static double activar;
-    static double version = 2.1;
+    static double version = 2.3;
 
     public static void main(String[] args) {
         // TODO code application logic here
@@ -36,9 +36,6 @@ public class Control_versiones {
         }
         if (activar == 1) {
             System.out.println("Cerrado");
-        }
-        if (activar > 1) {
-            System.out.println("PATRI PORFA SE MI COLABORADORA ;)  )");
         }
     }
 
