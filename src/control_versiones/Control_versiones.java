865355691aec202d0697f3dbee01d391c708a42c
fabrilalvarez <fabrilalvarez@eniac23.danielcastelao.org>
2015-01-26 08:52:45 +0100

    version 1

---
+++ src/control_versiones/Control_versiones.java
@@ -0,0 +1,12 @@
+
+package control_versiones;
+
+public class Control_versiones {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double version=1;
+        System.out.println("VERSION : "+version);
+    }
+    
+}
