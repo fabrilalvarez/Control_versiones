c400252cb2203be74668c222ec61a8e3ede23171
dpazolopez <dpazolopez@eniac21i.danielcastelao.org>
2015-01-29 09:39:06 +0100

    modificando clase de patricia

--- src/control_versiones/PatriMetodo.java
+++ src/control_versiones/PatriMetodo.java
@@ -5,7 +5,7 @@ package control_versiones;
 public class PatriMetodo {
     
     public static void chachiMetodo(){
-        System.out.println("Esto es un programa chachi");
+        System.out.println("Esto es un programa chachi pistachi");
     }
     
 }
